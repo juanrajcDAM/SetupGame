592e3b327718a208075b9a8eb0c4eb4d1baa9265
Juanra <juanrajc@hotmail.com>
2018-02-21 10:27:29 +0100

    NPCs añadidos.

--- core/src/com/pmdm/mijuego/MiJuego.java
+++ core/src/com/pmdm/mijuego/MiJuego.java
@@ -16,6 +16,7 @@ import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class MiJuego extends ApplicationAdapter implements InputProcessor {
@@ -71,6 +72,37 @@ public class MiJuego extends ApplicationAdapter implements InputProcessor {
 
     //Atributos que indican la anchura y altura del sprite animado del jugador.
     int anchoJugador, altoJugador;
+
+    //Animaciones posicionales relacionadas con los NPC del juego
+    private Animation noJugadorArriba;
+    private Animation noJugadorDerecha;
+    private Animation noJugadorAbajo;
+    private Animation noJugadorIzquierda;
+
+    //Array con los objetos Animation de los NPC
+    private Animation[] noJugador;
+
+    //Atributos que indican la anchura y altura del sprite animado de los NPC.
+    int anchoNoJugador, altoNoJugador;
+
+    //Posición inicial X de cada uno de los NPC
+    private float[] noJugadorX;
+
+    //Posición inicial Y de cada uno de los NPC
+    private float[] noJugadorY;
+
+    //Posición final X de cada uno de los NPC
+    private float[] destinoX;
+
+    //Posición final Y de cada uno de los NPC
+    private float[] destinoY;
+
+    //Número de NPC que van a aparecer en el juego
+    private static final int numeroNPCs = 20;
+
+    // Este atributo indica el tiempo en segundos transcurridos desde que se inicia la animación
+    // de los NPC , servirá para determinar cual es el frame que se debe representar.
+    private float stateTimeNPC;
 	
 	@Override
 	public void create () {
@@ -141,6 +173,73 @@ public class MiJuego extends ApplicationAdapter implements InputProcessor {
         anchoJugador =cuadroActual​​.getRegionWidth();
         altoJugador = cuadroActual​​.getRegionHeight();
 
+        //Inicializamos el apartado referente a los NPC
+        noJugador = new Animation[numeroNPCs];
+        noJugadorX = new float[numeroNPCs];
+        noJugadorY = new float[numeroNPCs];
+        destinoX = new float[numeroNPCs];
+        destinoY = new float[numeroNPCs];
+
+        //Creamos las animaciones posicionales de los NPC
+        //Cargamos la imagen de los frames del monstruo en el objeto img de la clase Texture.
+        img= new Texture(Gdx.files.internal("magorojo.png"));
+
+        //Sacamos los frames de img en un array de TextureRegion.
+        tmp = TextureRegion.split(img, img.getWidth() / FRAME_COLS, img.getHeight() /
+                FRAME_ROWS);
+        // Creamos las distintas animaciones, teniendo en cuenta que el tiempo de muestra de cada frame
+        // será de 150 milisegundos.
+        noJugadorArriba = new Animation(0.150f, tmp[0]);
+        noJugadorArriba.setPlayMode(Animation.PlayMode.LOOP);
+        noJugadorDerecha = new Animation(0.150f, tmp[1]);
+        noJugadorDerecha.setPlayMode(Animation.PlayMode.LOOP);
+        noJugadorAbajo = new Animation(0.150f, tmp[2]);
+        noJugadorAbajo.setPlayMode(Animation.PlayMode.LOOP);
+        noJugadorIzquierda = new Animation(0.150f, tmp[3]);
+        noJugadorIzquierda.setPlayMode(Animation.PlayMode.LOOP);
+
+        //Cargamos en los atributos del ancho y alto del sprite del monstruo sus valores
+        cuadroActual​​= (TextureRegion) noJugadorAbajo.getKeyFrame(stateTimeNPC);
+        anchoNoJugador = cuadroActual​​.getRegionWidth();
+        altoNoJugador = cuadroActual​​.getRegionHeight();
+
+        //Se inicializan, la animación por defecto y, de forma aleatoria, las posiciones
+        //iniciales y finales de los NPC. Para simplificar un poco, los NPC pares, se moveran
+        //de forma vertical y los impares de forma horizontal.
+        for (int i = 0; i < numeroNPCs; i++) {
+            noJugadorX[i] = (float) (Math.random() * anchoMapa);
+            noJugadorY[i] = (float) (Math.random() * altoMapa);
+
+            if (i % 2 == 0) {
+                // NPC par => mover de forma vertical
+                destinoX[i] = noJugadorX[i];
+                destinoY[i] = (float) (Math.random() * altoMapa);
+                // Determinamos cual de las animaciones verticales se utiliza.
+
+                if (noJugadorY[i] < destinoY[i]) {
+                    noJugador[i] = noJugadorArriba;
+                } else {
+                    noJugador[i] = noJugadorAbajo;
+                }
+
+            } else {
+                // NPC impar => mover de forma horizontal
+                destinoX[i] = (float) (Math.random() * anchoMapa);
+                destinoY[i] = noJugadorY[i];
+                // Determinamos cual de las animaciones horizontales se utiliza.
+
+                if (noJugadorX[i] < destinoX[i]) {
+                    noJugador[i] = noJugadorDerecha;
+                } else {
+                    noJugador[i] = noJugadorIzquierda;
+                }
+            }
+        }
+
+        // Ponemos a cero el atributo stateTimeNPC, que marca el tiempo e ejecución de la animación
+        // de los NPC.
+        stateTimeNPC = 0f;
+
 	}
 
 	private void capaObstaculos (TiledMapTileLayer capa){
@@ -202,8 +301,67 @@ public class MiJuego extends ApplicationAdapter implements InputProcessor {
 
 		batch.begin();
 		batch.draw(cuadroActual​​, jugadorX​, jugadorY​​); // 2
+
+        //Dibujamos las animaciones de los NPC
+        for (int i= 0; i < numeroNPCs; i++) {
+            actualizaNPC​​​​(i, 0.5f);
+            cuadroActual​​ = (TextureRegion) noJugador[i].getKeyFrame(stateTimeNPC);
+            batch.draw(cuadroActual​​,noJugadorX[i],noJugadorY[i]);
+        }
+
 		batch.end();
+
+        detectaColisiones​​();
 	}
+
+    //Método que permite cambiar las coordenadas del NPC en la posición "i",
+    //dada una variación "delta" en ambas coordenadas.
+    private void actualizaNPC​​​​(int i, float delta) {
+
+        if (destinoY[i]>noJugadorY[i]) {
+            noJugadorY[i] += delta;
+            noJugador[i] = noJugadorArriba;
+        }
+        if (destinoY[i]<noJugadorY[i]) {
+            noJugadorY[i] -= delta;
+            noJugador[i] = noJugadorAbajo;
+        }
+
+        if (destinoX[i]>noJugadorX[i]) {
+            noJugadorX[i] += delta;
+            noJugador[i] = noJugadorDerecha;
+        }
+        if (destinoX[i]<noJugadorX[i]) {
+            noJugadorX[i] -= delta;
+            noJugador[i] = noJugadorIzquierda;
+        }
+    }
+
+    private void detectaColisiones​​() {
+
+        //Vamos a comprobar que el rectángulo que rodea al jugador, no se solape
+        //con el rectángulo de alguno de los NPC. Primero calculamos el rectángulo
+        //en torno al jugador.
+        Rectangle rJugador=new Rectangle(jugadorX​, jugadorY​​, anchoJugador, altoJugador);
+
+        Rectangle rNPC;
+
+        //Ahora recorremos el array de NPC, para cada uno generamos su rectángulo envolvente
+        //y comprobamos si se solapa o no con el del Jugador.
+        for (int i=0; i<numeroNPCs; i++) {
+            rNPC = new Rectangle(noJugadorX[i], noJugadorY[i],
+                    anchoNoJugador,altoNoJugador);
+
+            //Se comprueba si se solapan.
+            if (rJugador.overlaps(rNPC)){
+                //hacer lo que haya que hacer en este caso, como puede ser reproducir un efecto
+                //de sonido, una animación del jugador alternativa y, posiblemente, que este muera
+                //y se acabe la partida actual. En principio, en este caso, lo único que se hace
+                //es mostrar un mensaje en la consola de texto.
+                System.out.println("Hay colisión!!!");
+            }
+        }
+    }
 	
 	@Override
 	public void dispose () {
@@ -329,7 +487,60 @@ public class MiJuego extends ApplicationAdapter implements InputProcessor {
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
+
+        // Vector en tres dimensiones que recoge las coordenadas donde se ha hecho click o toque de la pantalla.
+        Vector3 clickCoordinates = new Vector3(screenX, screenY, 0);
+
+        // Transformamos las coordenadas del vector a coordenadas de nuestra cámara.
+        Vector3 posicion = camara.unproject(clickCoordinates);
+
+        //​Se pone a cero el atributo que marca el tiempo de ejecución de la animación, provocando que la misma se reinicie.
+        stateTime​​ = 0;
+
+        /*​Guardamos la posición anterior del jugador por si al desplazarlo se topa
+        con un obstáculo y podamos volverlo a la posición anterior.*/
+        float jugadorAnteriorX = jugadorX​;
+        float jugadorAnteriorY = jugadorY​​;
+
+        /*Si se ha pulsado por encima de la animación, se sube
+        esta 5 píxeles y se reproduce la ​animación del jugador
+        desplazándose hacia arriba.
+         */
+        if ((jugadorY​​ +altoJugador) < posicion.y){
+            jugadorY​​ += 5;
+            jugador = jugadorArriba​​;
+        /*​Si se ha pulsado por debajo de la animación, se baja esta 5 píxeles y
+        se reproduce la animación del jugador desplazándose hacia abajo.*/
+        } else if((jugadorY​​) > posicion.y){
+            jugadorY​​ -= 5;
+            jugador = jugadorAbajo​​;
+        }
+
+        /*​Si se ha pulsado más de la mitad del ancho del sprite a la derecha
+        de la animación, se mueve esta 5 píxeles a la derecha se reproduce la
+        animación del jugador desplazándose hacia la derecha.*/
+        if((jugadorX​ + anchoJugador/2) < posicion.x){
+            jugadorX​ += 5;
+            jugador = jugadorDerecha​​;
+        /*Si se ha pulsado mas de la mitad del ancho del sprite a la izquierda
+        de laanimación, se mueve esta 5 píxeles a la izquierda y se reproduce la
+        animación del jugador desplazándose hacia la izquierda.*/
+        } else if ((jugadorX​ - anchoJugador/2) > posicion.x){
+            jugadorX​ -= 5;
+            jugador = jugadorIzquierda​​;
+        }
+
+        //​Detectamos las colisiones con los obstáculos del mapa y si el jugador se sale del mismo.
+        if((jugadorX​ < 0 || jugadorY​​ < 0 || jugadorX​ > (anchoMapa - anchoJugador) ||
+                jugadorY​​ > (altoMapa - altoJugador)) ||
+                ((obstaculo [(int) ((jugadorX​ + anchoJugador / 4) / anchoCelda)][((int) (jugadorY​​) / altoCelda)]) ||
+                        (obstaculo [(int) ((jugadorX​ + 3 * anchoJugador / 4) / anchoCelda)][((int) (jugadorY​​) / altoCelda)]))){
+            jugadorX​ = jugadorAnteriorX;
+            jugadorY​​ = jugadorAnteriorY;
+        }
+
+        return true;
+        
     }
 
     @Override
